bfe0a954689287e98c1ed165919d2ff5cd0551c7
Lucas Rasmussen <lucas.rasmussen+github@moz.com>
2023-07-16 14:45:24 -0700

    fix: ensure logo is a link to the home page

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,12 +1,15 @@
-import React from "react"
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Nav = () => {
   return (
     <header className="container p-3">
       <div className="flex items-center justify-between">
         <div className="flex-none flex items-center">
-          <Image src="/Logo.png" alt="SRA Logo" width="56" height="56" />
+          <Link href="/">
+            <Image src="/Logo.png" alt="SRA Logo" width="56" height="56" />
+          </Link>
           {/* <h1 className="p-3">Soren Rasmussen Architects</h1> */}
         </div>
         {/* <div className="">
@@ -25,7 +28,7 @@ const Nav = () => {
         </div> */}
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
