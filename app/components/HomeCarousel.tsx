2228d9a8f5379e8dafb37262e952eb2ad18878ec
Lucas Rasmussen <lucas.rasmussen+github@moz.com>
2023-07-16 14:37:25 -0700

    fix: add third slide and add autoplay

--- app/components/HomeCarousel.tsx
+++ app/components/HomeCarousel.tsx
@@ -1,22 +1,22 @@
-"use client"
-import React from "react"
-import Carousel from "react-multi-carousel"
-import { ButtonGroupProps, ArrowProps } from "react-multi-carousel/lib/types"
-import "react-multi-carousel/lib/styles.css"
+"use client";
+import React from "react";
+import Carousel from "react-multi-carousel";
+import { ButtonGroupProps, ArrowProps } from "react-multi-carousel/lib/types";
+import "react-multi-carousel/lib/styles.css";
 import {
   FontAwesomeIcon,
   FontAwesomeIconProps,
-} from "@fortawesome/react-fontawesome"
-import Image from "next/image"
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons"
+} from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 interface CarouselButtonGroupProps extends ButtonGroupProps {
-  className?: string
+  className?: string;
 }
 
 interface CarouselArrowProps extends ArrowProps {
-  className?: string
-  arrowSize?: FontAwesomeIconProps["size"]
+  className?: string;
+  arrowSize?: FontAwesomeIconProps["size"];
 }
 
 const HomeCarousel = () => {
@@ -38,9 +38,20 @@ const HomeCarousel = () => {
       breakpoint: { max: 464, min: 0 },
       items: 1,
     },
-  }
+  };
 
   const slides: { image: JSX.Element; caption: React.ReactNode }[] = [
+    {
+      image: (
+        <Image
+          alt=""
+          src="/carousel/sunshine-coast.jpg"
+          width={1800}
+          height={1200}
+        />
+      ),
+      caption: "Award winning architecture studio",
+    },
     {
       image: (
         <Image src="/carousel/sobre.jpg" alt="" width={1800} height={1200} />
@@ -58,7 +69,7 @@ const HomeCarousel = () => {
       ),
       caption: "Internationally recognized for creativity & originality",
     },
-  ]
+  ];
 
   const CustomLeftArrow = ({
     onClick,
@@ -69,8 +80,8 @@ const HomeCarousel = () => {
       <button className={className} onClick={() => onClick && onClick()}>
         <FontAwesomeIcon icon={faArrowLeft} size={arrowSize} />
       </button>
-    )
-  }
+    );
+  };
 
   const CustomRightArrow = ({
     onClick,
@@ -81,15 +92,15 @@ const HomeCarousel = () => {
       <button className={className} onClick={() => onClick && onClick()}>
         <FontAwesomeIcon icon={faArrowRight} size={arrowSize} />
       </button>
-    )
-  }
+    );
+  };
 
   const CustomButtonGroupAsArrows = ({
     next,
     previous,
   }: CarouselButtonGroupProps) => {
     const buttonClassName =
-      "text-white border-white border-2 rounded-full w-10 h-10 hover:text-zinc-200 hover:border-zinc-200"
+      "text-white border-white border-2 rounded-full w-10 h-10 hover:text-zinc-200 hover:border-zinc-200";
     return (
       <div className="absolute right-3 bottom-3 ">
         <CustomLeftArrow
@@ -103,29 +114,27 @@ const HomeCarousel = () => {
           arrowSize="lg"
         />
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <Carousel
-      // customLeftArrow={<CustomLeftArrow />}
-      // customRightArrow={<CustomRightArrow />}
       responsive={responsive}
       customButtonGroup={<CustomButtonGroupAsArrows />}
       arrows={false}
       infinite={true}
-      // customDot={<CustomDots />}
+      autoPlay={true}
     >
       {slides.map((slide, index) => (
         <div className="relative" key={index}>
           {slide.image}
-          <p className="legend absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl md:text-3xl lg:text-4xl text-center min-w-full">
+          <p className="legend absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl md:text-3xl lg:text-4xl text-center min-w-full uppercase">
             {slide.caption}
           </p>
         </div>
       ))}
     </Carousel>
-  )
-}
+  );
+};
 
-export default HomeCarousel
+export default HomeCarousel;
