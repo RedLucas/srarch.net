69e078cb37d806905c4bfed42be69d83d3acdc98
Lucas Rasmussen <lucas.rasmussen+github@moz.com>
2023-07-16 15:10:36 -0700

    fix: change external links to regular anchor tags

--- app/memory/page.tsx
+++ app/memory/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { MemoryVideo } from "../components/MemoryVideo";
-import Link from "next/link";
 
 export default function Memory() {
   return (
@@ -14,25 +13,25 @@ export default function Memory() {
         <p className="pb-5">
           The first stage is depicted by &quot;Throwing it all away&quot; by
           Genesis. It represents the time before the birth of his sons.{" "}
-          <Link href="https://music.apple.com/ca/album/throwing-it-all-away/396483774?i=396483957">
+          <a href="https://music.apple.com/ca/album/throwing-it-all-away/396483774?i=396483957">
             Link to download
-          </Link>
+          </a>
         </p>
         <p className="pb-5">
           The second stage is also sung by Phil Collins, &quot;You&apos;ll be in
           my heart&quot;. It represents the time he spent raising his kids.{" "}
-          <Link href="https://music.apple.com/ca/album/youll-be-in-my-heart/1437333655?i=1437333914">
+          <a href="https://music.apple.com/ca/album/youll-be-in-my-heart/1437333655?i=1437333914">
             Link to download
-          </Link>
+          </a>
         </p>
         <p>
           The final stage is &quot;Take it easy&quot; by The Eagles. It
           represents a time of his life that was unfortunately cut short. A time
           that he spent enjoying the life he built for himself, even though
           actually <em>taking it easy</em> was difficult to actually accomplish.{" "}
-          <Link href="https://music.apple.com/ca/album/take-it-easy/635788137?i=635788139">
+          <a href="https://music.apple.com/ca/album/take-it-easy/635788137?i=635788139">
             Link to download
-          </Link>
+          </a>
         </p>
       </div>
       <div className="col-span-2">
