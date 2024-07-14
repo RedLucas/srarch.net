0a55888c26518a3cd08609db2c6f258138828847
Lucas Rasmussen <lucas.rasmussen@moz.com>
2024-07-14 16:36:31 -0700

    feat: move to edge from static

--- app/memory/page.tsx
+++ app/memory/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { MemoryVideo } from "../components/MemoryVideo";
 
+export const runtime = "edge";
+
 export default function Memory() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
